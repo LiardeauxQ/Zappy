7a41f9b53722a71ef07deba0f3121fc5e5c83aec
herbosa <antoine.herbosa@epitech.eu>
2019-06-14 23:44:37 +0200

    feat:elevation:add dictionary for elevation nb of players

--- AI/sources/main.py
+++ AI/sources/main.py
@@ -1,9 +1,12 @@
 from sources.ressources.stone import *
 from sources.ressources.stonesTypes import *
 from sources.ressources.dictionaries import *
+from sources.elevation.playerNeededForElevation import *
+
 
 if __name__ == "__main__":
     s = Stone(StonesTypes.LINEMATE)
     
     s.logStone()
     print("LINEMATE desirable at level 1 =", level_1_desirable[StonesTypes.LINEMATE])
+    print("From level 2 to level 3 you need", playerNeededForElevation[2], "players")
