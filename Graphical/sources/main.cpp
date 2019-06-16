22a8db3f909d76d3edc3f7637faba1e85ff16b48
Kilian Perrier <kilian.perrier@epitech.eu>
2019-06-16 20:48:35 +0200

    feat: Graphical: Graphical client now display a 50x50 empty tiles grid.

--- Graphical/sources/main.cpp
+++ Graphical/sources/main.cpp
@@ -8,6 +8,7 @@
 #include "InputParser.hpp"
 #include "ServerInteraction.hpp"
 #include "Core/Window.hpp"
+#include "Tile.hpp"
 
 int main(int ac, char **av)
 {
@@ -19,5 +20,15 @@ int main(int ac, char **av)
 //    interaction.requestTileContent();
 //    interaction.requestMapContent();
     zapi::Window window("Zappy");
+
+    for (int i = 0, x = 0, y = 0; i != 2500; i++, x += 100) {
+        if (x >= 5000) {
+            x = 0;
+            y += 100;
+        }
+        window.addEntity(new zapi::Tile(sf::Vector2f(100, 100), sf::Vector2f(x, y)));
+    }
+    
+    window.startLoop();
     return (0);
 }
