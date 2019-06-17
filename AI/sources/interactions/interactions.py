607a7ea4942af2ffa0dee99885ffa1a52507e689
Niels <niels.george-peillex@epitech.eu>
2019-06-17 16:08:43 +0200

    feat:interactions: create Player class to interaction with the client (command)

---
+++ AI/sources/interactions/interactions.py
@@ -0,0 +1,42 @@
+from sources.socket.socket import *
+
+class Player:
+
+    def __init__(self):
+        self.client = Socket()
+
+    def forward(self):
+        self.client.sendMessage("Forward\n")
+
+    def right(self):
+        self.client.sendMessage("Right\n")
+
+    def left(self):
+        self.client.sendMessage("Left\n")
+
+    def look(self):
+        self.client.sendMessage("Look\n")
+
+    def inventory(self):
+        self.client.sendMessage("Inventory\n")
+
+    def broadcast(self):
+        self.client.sendMessage("Broadcast text\n")
+
+    def connectNumber(self):
+        self.client.sendMessage("Connect_nbr\n")
+
+    def fork(self):
+        self.client.sendMessage("Fork\n")
+
+    def eject(self):
+        self.client.sendMessage("Eject\n")
+
+    def takeObject(self):
+        self.client.sendMessage("Take object\n")
+
+    def dropObject(self):
+        self.client.sendMessage("Set object\n")
+
+    def startIncantation(self):
+        self.client.sendMessage("Incantation\n")
