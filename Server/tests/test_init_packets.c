c36c5313cc977babcfcea07c8cd2ede3f7122a0c
Quentin <quentin.liardeaux@epitech.eu>
2019-06-17 15:28:31 +0200

    test: Server: Resove error on init_packets test
    Update protocol to v.7

--- Server/tests/test_init_packets.c
+++ Server/tests/test_init_packets.c
@@ -16,13 +16,14 @@ Test(init_client_packets, simple_test)
     cr_assert_eq(reg.size, 11);
 }
 
-Test(init_client_packets, error_test_with_no_value)
+Test(init_client_packets, test_with_no_value)
 {
     phr_t reg = {0};
 
     cr_assert_eq(init_client_packets(&reg), 0);
     cr_assert_eq(init_server_environnements_packets(NULL), -1);
-    cr_assert_eq(init_server_actions_packets(NULL), -1);
+    cr_assert_eq(init_server_actions_packets1(NULL), -1);
+    cr_assert_eq(init_server_actions_packets2(NULL), -1);
     cr_assert_eq(init_server_additionals_packets(NULL), -1);
 }
 
@@ -42,12 +43,11 @@ Test(init_server_actions_packets1, simple_test)
     phr_t reg = {0};
 
     init_server_actions_packets1(&reg);
-    cr_assert_eq(reg.size, 13);
+    cr_assert_eq(reg.size, 5);
     cr_assert_eq(reg.handlers[0]->subid, 0);
     cr_assert_eq(reg.handlers[1]->subid, 0);
     cr_assert_eq(reg.handlers[2]->subid, 0);
     cr_assert_eq(reg.handlers[3]->subid, 0);
-    cr_assert_eq(init_server_actions_packets1(0x0), -1);
 }
 
 Test(init_server_actions_packets2, simple_test)
@@ -55,11 +55,10 @@ Test(init_server_actions_packets2, simple_test)
     phr_t reg = {0};
 
     init_server_actions_packets2(&reg);
-    cr_assert_eq(reg.size, 13);
+    cr_assert_eq(reg.size, 8);
     cr_assert_str_eq(reg.handlers[0]->name, "SRV_PLAYER_DEATH");
     cr_assert_str_eq(reg.handlers[1]->name, "SRV_EGG_LAYED");
     cr_assert_str_eq(reg.handlers[2]->name, "SRV_EGG_HATCHING");
-    cr_assert_eq(init_server_actions_packets2(0x0), -1);
 }
 
 Test(init_server_additionals_packets, simple_test)
