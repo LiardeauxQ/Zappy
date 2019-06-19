7b4b706b32af246f5d5156fac9d9b10467eec82e
Quentin <quentin.liardeaux@epitech.eu>
2019-06-19 20:17:17 +0200

    test: Server: Add test for tile_content graphical procotol command
    Fix errors in send_tile_content and send_map_size functions
    Add functions that wrap protocols commands in unit tests

--- Server/tests/commands/test_map_size.c
+++ Server/tests/commands/test_map_size.c
@@ -16,29 +16,25 @@
 #include "graphical/protocols.h"
 #include "world.h"
 #include "linked_list.h"
+#include "unit_tests.h"
 
 Test(send_map_size, simple_test)
 {
     world_t world = {0};
     int fd = open("tmp", O_CREAT | O_RDWR, 0644);
-    int magic_num = SENDER_MAGIC_NUM;
-    char data[80] = {0};
     char buffer[80] = {0};
-    const size_t size = PKT_HDR_LEN + SRV_MAP_SIZE_LEN + 1;
+    const size_t size = PKT_HDR_LEN + SRV_MAP_SIZE_LEN;
     char result[PKT_HDR_LEN + SRV_MAP_SIZE_LEN + 1] = {0};
     pkt_header_t hdr = {13, PROTOCOL_VERSION, SRV_MAP_SIZE_LEN, 0};
     srv_map_size_t map_size = {34, 34};
-    sender_t sender = {&world, sizeof(world_t), fd, 1};
+    cmd_info_t cmd[] = {{&world, sizeof(world_t)}};
 
     cr_assert_neq(fd, -1);
     world.width = 34;
     world.height = 34;
-    memcpy(data, &magic_num, 4);
-    memcpy(data + 4, &sender, sizeof(sender_t));
+    wrap_graph_protocol_commands(&send_map_size, fd, cmd);
     memcpy(result, &hdr, PKT_HDR_LEN);
     memcpy(result + PKT_HDR_LEN, &map_size, SRV_MAP_SIZE_LEN);
-    send_map_size(data);
-    lseek(fd, 0, SEEK_SET);
     read(fd, buffer, size);
     for (size_t i = 0 ; i < size ; i++)
         cr_assert_eq(buffer[i], result[i]);
