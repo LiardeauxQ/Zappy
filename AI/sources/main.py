68d0277d0459d6e0d5aa7e9329bed46f22adc5ea
Niels <niels.george-peillex@epitech.eu>
2019-06-20 21:58:02 +0200

    fix:sources: main architecrue creating a player and launch his loop

--- AI/sources/main.py
+++ AI/sources/main.py
@@ -5,10 +5,9 @@ from AI.sources.elevation.playerNeededForElevation import *
 from AI.sources.socket.socket import *
 from AI.sources.interactions.interactions import *
 
-
 if __name__ == "__main__":
+
     client = Player()
 
     client.client.sendMessage("abc\n");
-    client.look();
-
+    client.start();
