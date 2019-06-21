617675474a952322fb1de1eba360ca46c16c460c
Kilian Perrier <kilian.perrier@epitech.eu>
2019-06-21 15:20:12 +0200

    fix: Graphical: shaders path fixed.

--- Graphical/GraphicalAPI/sources/Core/Window.cpp
+++ Graphical/GraphicalAPI/sources/Core/Window.cpp
@@ -15,7 +15,7 @@ zapi::Window::Window(const std::string &title)
 {
     setView(camera);
     shadow.setUniform("u_resolution", sf::Glsl::Vec2(100, 100));
-    shadow.loadFromFile("/home/night/tek/tek2/PSU_zappy_2018/Graphical/shaders/shadow.frag", sf::Shader::Fragment);
+    shadow.loadFromFile("shaders/shadow.frag", sf::Shader::Fragment);
 }
 
 void zapi::Window::update()
