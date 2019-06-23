90753a445fe4a61d7a66a3c0596e8af6c5c45d15
herbosa <antoine.herbosa@epitech.eu>
2019-06-23 03:14:49 +0200

    WIP:graphic:menu:handle event for menu

--- Graphical/GraphicalAPI/includes/Core/Menu.hpp
+++ Graphical/GraphicalAPI/includes/Core/Menu.hpp
@@ -20,8 +20,26 @@ namespace zapi
             ~Menu() = default;
             void start();
             void loop();
+            void event();
 
 
         private:
+            sf::RenderWindow window;
+            sf::Texture texture;
+            sf::String playerInput; 
+            sf::Texture buttonTexture;
+            sf::RectangleShape rectangleButton;
+            sf::IntRect rectSourceSprite;
+            sf::Sprite buttonSprite;
+            sf::Sprite sprite;
+            sf::RectangleShape rectangle;
+            std::string inputHostStr;
+            std::string inputPortStr;
+            sf::Text inputHost;
+            sf::Text inputPort;
+            sf::Font font;
+            bool clicked;
+
+
     };
 }
--- Graphical/GraphicalAPI/sources/Core/Menu.cpp
+++ Graphical/GraphicalAPI/sources/Core/Menu.cpp
@@ -12,7 +12,33 @@
 #include <SFML/Graphics.hpp>
 
 zapi::Menu::Menu()
+    : window(sf::VideoMode(1920, 1080), "Zappy"),
+    rectangleButton(sf::Vector2f(190, 47)),
+    rectSourceSprite(0, 0, 190, 47),
+    rectangle(sf::Vector2f(120, 50)),
+    inputHostStr("Bite"),
+    inputPortStr("Bite")
+
 {
+        if (!buttonTexture.loadFromFile("Graphical/sprites/yellowSheet.png"))
+            return;
+        if (!texture.loadFromFile("Graphical/sprites/menu.png"))
+            return;
+        buttonSprite = sf::Sprite(buttonTexture,rectSourceSprite);
+        sprite = sf::Sprite(texture);
+        font.loadFromFile("Graphical/GraphicalAPI/sources/Core/font.ttf");
+        
+        rectangle.setSize(sf::Vector2f(800, 200));
+        inputHost.setCharacterSize(68);
+        inputPort.setCharacterSize(68);
+        inputHost.setColor(sf::Color::Red);
+        inputPort.setColor(sf::Color::Red);
+        inputPort.setPosition(500, 500);
+        inputHost.setFont(font);
+        inputPort.setFont(font);
+        clicked = false;
+
+
 }
 
 
@@ -23,13 +49,6 @@ void zapi::Menu::start()
 
 void zapi::Menu::loop()
 {
-    sf::RenderWindow window(sf::VideoMode(1920, 1080), "Zappy");
-    sf::Texture texture;
-    sf::String playerInput;
-
-    if (!texture.loadFromFile("Graphical/sprites/menu.png"))
-        return;
-    sf::Sprite sprite(texture);
     while (window.isOpen())
     {
         sf::Event event;
@@ -37,9 +56,43 @@ void zapi::Menu::loop()
         {
             if (event.type == sf::Event::Closed)
                 window.close();
+            if (rectangleButton.getGlobalBounds().contains(window.mapPixelToCoords(sf::Mouse::getPosition(window)))) {
+                if (sf::Mouse::isButtonPressed(sf::Mouse::Button::Left)) {
+                    rectSourceSprite.top = 94;
+                } else if (rectSourceSprite.top == 0)
+                    rectSourceSprite.top = 47;
+                } else
+                    rectSourceSprite.top = 0;
+
+            if (sf::Mouse::isButtonPressed(sf::Mouse::Button::Left)) {
+                if (rectangle.getGlobalBounds().contains(window.mapPixelToCoords(sf::Mouse::getPosition(window))))
+                    clicked = true;
+                else
+                    clicked = false;
+            if (sf::Mouse::isButtonPressed(sf::Mouse::Button::Left)) {
+                if (rectangle.getGlobalBounds().contains(window.mapPixelToCoords(sf::Mouse::getPosition(window))))
+                    clicked = true;
+                else
+                    clicked = false;
+            }
+            if (event.type == sf::Event::TextEntered && clicked == true)
+                if (event.text.unicode >= 48 && event.text.unicode <= 57)
+                    inputPortStr +=  (static_cast<char>(event.text.unicode));
+            }
+            if (event.type == sf::Event::TextEntered && clicked == true)
+                if (event.text.unicode < 128)
+                    inputHostStr += (static_cast<char>(event.text.unicode));
+            inputHost.setString(inputHostStr);
+            inputPort.setString(inputPortStr);
+
         }
+        buttonSprite.setTextureRect(rectSourceSprite);
         window.clear();
         window.draw(sprite);
+        window.draw(rectangle);
+        window.draw(inputHost);
+        window.draw(inputPort);
+    //    window.draw(buttonSprite);
         window.display();
     }
 }
