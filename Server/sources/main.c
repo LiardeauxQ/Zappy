dbb046609e724c2dfc79147b6d9bf472928969b4
Quentin <quentin.liardeaux@epitech.eu>
2019-06-23 22:35:44 +0200

    feat: Server: print message when SIGINT is raised

--- Server/sources/main.c
+++ Server/sources/main.c
@@ -23,6 +23,7 @@ static info_t info = {0};
 
 void sigint_handler(int __attribute__((unused)) sig) {
     destroy_info(&info);
+    printf("The server has been closed\n");
     exit(0);
 }
 
@@ -31,21 +32,19 @@ void check_connection(game_t *game, server_t *server, client_reader reader)
     struct timeval timeout = {0};
     int max_fd = 0;
     int sockfd = server->sockfd;
-    fd_set readfds = server->readfds;
+    fd_set *readfds = &server->readfds;
     int clt_sockfd = 0;
 
     timeout.tv_usec = 1000;
-    max_fd = set_fds(&readfds, server->clients, sockfd);
-    if (select(max_fd + 1, &readfds, 0x0, 0x0, &timeout) == -1)
+    max_fd = set_fds(readfds, server->clients, sockfd);
+    if (select(max_fd + 1, readfds, 0x0, 0x0, &timeout) == -1)
         exit_with_error("select");
-    clt_sockfd = get_new_connection(&readfds, &server->clients, sockfd);
-    if (clt_sockfd > 0)
-        printf("socket %d %p\n", clt_sockfd, reader);
+    clt_sockfd = get_new_connection(readfds, &server->clients, sockfd);
     if (reader == &read_graph_client)
         send_graph_welcome(&game->world, server->clients, clt_sockfd);
     if (clt_sockfd > 0 && reader == &read_ai_client)
         write(clt_sockfd, WELCOME_MSG, WELCOME_MSG_LEN);
-    handle_clients(game, &server->clients, &readfds, reader);
+    handle_clients(game, &server->clients, readfds, reader);
 }
 
 int start_server(info_t *info)
